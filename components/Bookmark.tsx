8f83d9dfd87b3c9f7ff46744163aae588da12890
Naman Sharma <namansharmans03@gmail.com>
2026-04-04 23:04:51 +0530

    setup

---
+++ components/Bookmark.tsx
@@ -0,0 +1,17 @@
+"use client"
+import React from 'react'
+import { motion } from 'framer-motion';
+const Bookmark = () => {
+  return (
+    <div>
+<svg width="149" height="170" viewBox="0 0 149 170" fill="none" xmlns="http://www.w3.org/2000/svg">
+<rect width="93.6021" height="93.6021" transform="matrix(0.795922 0.605399 -0.795922 0.605399 74.5 0)" fill="white"/>
+<rect width="93.6021" height="56.6667" transform="matrix(0.795922 0.605399 0 1 0 56.3334)" fill="#D9D9D9"/>
+<rect width="93.6021" height="56.6667" transform="matrix(0.795922 -0.605399 0 1 74.5 113)" fill="#B3B3B3"/>
+</svg>
+
+    </div>
+  )
+}
+
+export default Bookmark

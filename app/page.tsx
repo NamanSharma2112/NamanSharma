b10707fdffa5e73471e56fc26b74d1c07eeb8b79
Naman Sharma <namansharmans03@gmail.com>
2026-04-08 23:10:33 +0530

    updated my animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
+"use client"
 import Bookmark from "@/components/Bookmark";
+import { HoldToConfirmFinal } from "@/components/Delete";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-100 font-sans dark:bg-black">
-    <Bookmark />
+  
+     <HoldToConfirmFinal
+        text="Delete project"
+        onConfirm={() => alert('Poof! Deleted.')}
+      />
     </div>
   );
 }
